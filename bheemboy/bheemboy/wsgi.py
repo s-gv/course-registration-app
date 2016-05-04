acd1d81b346a4a407550a5a00292c5ad5762d0c6
Sagar G V <sagar.writeme@gmail.com>
2016-05-04 19:00:11 +0530

    django project name changed

--- bheemboy/bheemboy/wsgi.py
+++ bheemboy/bheemboy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bheemBoy project.
+WSGI config for bheemboy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bheemBoy.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bheemboy.settings")
 
 application = get_wsgi_application()
