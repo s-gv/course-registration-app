8e691e077e42c1b7df2330d0e976c5bd1c75e033
Sagar G V <sagar.writeme@gmail.com>
2016-05-06 16:31:20 +0530

    Trimmed the admin page for User

--- bheemboy/coursereg/admin.py
+++ bheemboy/coursereg/admin.py
@@ -14,8 +14,6 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal info'), {'fields': ('full_name', 'user_type', 'adviser', 'program', 'sr_no')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
