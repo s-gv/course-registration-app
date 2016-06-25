604d644843bd3727e8eac7d09c7ed0ba7e71cadb
Sagar G V <sagar.writeme@gmail.com>
2016-06-25 17:42:00 +0530

    Student page re-factored

--- coursereg/views/participants.py
+++ coursereg/views/participants.py
@@ -1,9 +1,55 @@
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.utils import timezone
+from datetime import timedelta
+from coursereg import models
 
+@login_required
 def create(request):
-    pass
+    assert request.method == 'POST'
+    course_id = request.POST['course_id']
+    user_id = request.POST['user_id']
+    reg_type = request.POST['reg_type']
+    assert str(user_id) == str(request.user.id)
 
+    state = models.Participant.STATE_CREDIT
+    if reg_type == 'audit':
+        state = models.Participant.STATE_AUDIT
+
+    if not course_id:
+        messages.error(request, 'Select a course.')
+    else:
+        course = models.Course.objects.get(id=course_id)
+        if models.Participant.objects.filter(user__id=user_id, course__id=course_id):
+            messages.error(request, 'Already registered for %s.' % course)
+        elif timezone.now().date() > course.last_reg_date:
+            messages.error(request, 'Registration for %s is now closed.' % course)
+        else:
+            models.Participant.objects.create(
+                user_id=user_id,
+                course_id=course_id,
+                participant_type=models.Participant.PARTICIPANT_STUDENT,
+                state=state,
+                grade=models.Participant.GRADE_NA,
+                is_adviser_approved=False,
+                instructor_state=models.Participant.INSTRUCTOR_PENDING
+            )
+            messages.success(request, 'Successfully applied for %s.' % course)
+
+    return redirect(request.GET.get('next', reverse('coursereg:index')))
+
+@login_required
 def update(request):
     pass
 
-def delete(request):
-    pass
+@login_required
+def delete(request, participant_id):
+    assert request.method == 'POST'
+    participant = models.Participant.objects.get(id=participant_id)
+    assert str(participant.user.id) == str(request.user.id)
+    participant.delete()
+    return redirect(request.GET.get('next', reverse('coursereg:index')))
--- coursereg/views/student.py
+++ coursereg/views/student.py
@@ -9,17 +9,36 @@ from datetime import timedelta
 from coursereg import models
 from django.contrib.auth import update_session_auth_hash
 
+def get_desc(participant):
+    if participant.state  == models.Participant.STATE_DROP:
+        return 'Dropped out of this course'
+    if not participant.is_adviser_approved:
+        return 'Awaiting adviser review'
+    if participant.instructor_state == models.Participant.INSTRUCTOR_PENDING:
+        return 'Adviser has approved your application. Awaiting instructor review'
+    elif participant.instructor_state == models.Participant.INSTRUCTOR_REJECTED:
+        return 'Course instructor has rejected your application'
+    elif participant.instructor_state == models.Participant.INSTRUCTOR_APPROVED:
+        if participant.grade != models.Participant.GRADE_NA:
+            return participant.grade + ' grade'
+        else:
+            return 'Enrolled in course'
+
+@login_required
 def index(request):
     if not request.user.adviser:
         messages.error(request, 'Adviser not assigned. Contact the administrator.')
         return redirect('coursereg:fatal')
+
+    participants = [(p.id, p.state, p.course, get_desc(p), not p.is_adviser_approved)
+        for p in models.Participant.objects.filter(user=request.user).order_by('-course__last_reg_date', 'course__title')]
 
     context = {
         'user_type': 'student',
         'nav_active': 'home',
 		'user_email': request.user.email,
         'user_id': request.user.id,
-        'participants': None,
+        'participants': participants,
         'courses': models.Course.objects.filter(last_reg_date__gte=timezone.now(),
                                                 last_reg_date__lte=timezone.now()+timedelta(days=100)),
         'remarks': request.user.dcc_remarks,
