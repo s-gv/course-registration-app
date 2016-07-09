02580dda08ecb1384cacbbf77d84c068ea5bd4ce
Sagar G V <sagar.writeme@gmail.com>
2016-07-09 13:14:44 +0530

    Removed custom create_user method

--- coursereg/tests/test_user.py
+++ coursereg/tests/test_user.py
@@ -2,16 +2,16 @@ from django.test import TestCase
 from django.test import Client
 from django.core.urlresolvers import reverse
 from coursereg.models import User
-import utils
+from utils import is_error_msg_present
 
 class StudentLoginTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        dcc = utils.create_user('dcc@test.com', 'dcc12345', User.USER_TYPE_DCC)
-        admin = utils.create_user('admin@test.com', 'admin12345', User.USER_TYPE_OTHER, is_superuser=True)
-        charles = utils.create_user('charles@test.com', 'charles12345', User.USER_TYPE_FACULTY)
-        alyssa = utils.create_user('alyssa@test.com', 'alyssa12345', User.USER_TYPE_STUDENT, adviser=charles)
-        ben = utils.create_user('ben@test.com', 'ben12345', User.USER_TYPE_STUDENT)
+        dcc = User.objects.create_user(email='dcc@test.com', password='dcc12345', user_type=User.USER_TYPE_DCC)
+        admin = User.objects.create_superuser(email='admin@test.com', password='admin12345', user_type=User.USER_TYPE_OTHER)
+        charles = User.objects.create_user(email='charles@test.com', password='charles12345', user_type=User.USER_TYPE_FACULTY)
+        alyssa = User.objects.create_user(email='alyssa@test.com', password='alyssa12345', user_type=User.USER_TYPE_STUDENT, adviser=charles)
+        ben = User.objects.create_user(email='ben@test.com', password='ben12345', user_type=User.USER_TYPE_STUDENT)
 
     def setUp(self):
         self.client = Client()
@@ -20,7 +20,7 @@ class StudentLoginTests(TestCase):
         response = self.client.post(reverse('coursereg:signin'), {'email': 'ben@test.com', 'password': 'blahblah'}, follow=True)
         self.assertTemplateUsed(response, 'coursereg/signin.html')
         self.assertRedirects(response, reverse('coursereg:signin'))
-        self.assertEquals(utils.is_error_msg_present(response), True)
+        self.assertEquals(is_error_msg_present(response), True)
 
     def test_was_student_without_adviser_shown_fatal_error(self):
         response = self.client.post(reverse('coursereg:signin'), {'email': 'ben@test.com', 'password': 'ben12345'}, follow=True)
@@ -40,4 +40,4 @@ class StudentLoginTests(TestCase):
 
     def test_was_superuser_shown_right_index_page(self):
         response = self.client.post(reverse('coursereg:signin'), {'email': 'admin@test.com', 'password': 'admin12345'}, follow=True)
-        self.assertTemplateUsed(response, 'admin/login.html')
+        self.assertTemplateUsed(response, 'admin/index.html')
--- coursereg/tests/utils.py
+++ coursereg/tests/utils.py
@@ -1,13 +1,6 @@
-from coursereg.models import User
 from django.contrib import messages
 
 def is_error_msg_present(response):
     for message in response.context['messages']:
         if message.level == messages.ERROR:
             return True
-
-def create_user(email, password, user_type, adviser=None, is_superuser=False):
-    u = User.objects.create(email=email, user_type=user_type, adviser=adviser, is_superuser=is_superuser)
-    u.set_password(password)
-    u.save()
-    return u
