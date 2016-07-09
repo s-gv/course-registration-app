64b8d402dc43f15a9ded68619e14e075f0c2bf4d
Sagar G V <sagar.writeme@gmail.com>
2016-07-09 13:09:06 +0530

    Share data across tests

--- coursereg/tests/test_user.py
+++ coursereg/tests/test_user.py
@@ -5,11 +5,16 @@ from coursereg.models import User
 import utils
 
 class StudentLoginTests(TestCase):
-    def setUp(self):
-        self.client = Client()
+    @classmethod
+    def setUpTestData(cls):
+        dcc = utils.create_user('dcc@test.com', 'dcc12345', User.USER_TYPE_DCC)
+        admin = utils.create_user('admin@test.com', 'admin12345', User.USER_TYPE_OTHER, is_superuser=True)
         charles = utils.create_user('charles@test.com', 'charles12345', User.USER_TYPE_FACULTY)
         alyssa = utils.create_user('alyssa@test.com', 'alyssa12345', User.USER_TYPE_STUDENT, adviser=charles)
         ben = utils.create_user('ben@test.com', 'ben12345', User.USER_TYPE_STUDENT)
+
+    def setUp(self):
+        self.client = Client()
 
     def test_was_login_with_wrong_password_rejected(self):
         response = self.client.post(reverse('coursereg:signin'), {'email': 'ben@test.com', 'password': 'blahblah'}, follow=True)
@@ -25,31 +30,13 @@ class StudentLoginTests(TestCase):
         response = self.client.post(reverse('coursereg:signin'), {'email': 'alyssa@test.com', 'password': 'alyssa12345'}, follow=True)
         self.assertTemplateUsed(response, 'coursereg/student.html')
 
-
-class FacultyLoginTests(TestCase):
-    def setUp(self):
-        self.client = Client()
-        charles = utils.create_user('charles@test.com', 'charles12345', User.USER_TYPE_FACULTY)
-
     def test_was_faculty_shown_right_index_page(self):
         response = self.client.post(reverse('coursereg:signin'), {'email': 'charles@test.com', 'password': 'charles12345'}, follow=True)
         self.assertTemplateUsed(response, 'coursereg/adviser.html')
 
-
-class DCCLoginTests(TestCase):
-    def setUp(self):
-        self.client = Client()
-        dcc = utils.create_user('dcc@test.com', 'dcc12345', User.USER_TYPE_DCC)
-
     def test_was_dcc_shown_right_index_page(self):
         response = self.client.post(reverse('coursereg:signin'), {'email': 'dcc@test.com', 'password': 'dcc12345'}, follow=True)
         self.assertTemplateUsed(response, 'coursereg/dcc.html')
-
-
-class SuperUserLoginTests(TestCase):
-    def setUp(self):
-        self.client = Client()
-        admin = utils.create_user('admin@test.com', 'admin12345', User.USER_TYPE_OTHER, is_superuser=True)
 
     def test_was_superuser_shown_right_index_page(self):
         response = self.client.post(reverse('coursereg:signin'), {'email': 'admin@test.com', 'password': 'admin12345'}, follow=True)
