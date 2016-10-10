484de28c1a37803ae309f49220267cffaaa2ddd2
Srivatsa Bhargava J <bhargavajs.07@gmail.com>
2016-10-10 08:53:39 +0530

    Reverting an unnecessary change. 	modified:   manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-sys.path.append('/home/bhargava/git/bheemboy/courseRegrWebSite/courseRegrEnv/lib/python2.7/site-packages')
 
 
 if __name__ == "__main__":
